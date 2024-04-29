e446a9f02b1d662a391c0479a5ed9c16a27ed8d7
Monica <monibrave1@gmail.com>
2024-04-29 18:10:35 -0300

    agregando ejercicios de estructura de selectiva

---
+++ Descuentos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Descuento
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //Un negocio efectúa descuentos según el importe. Menor a $100 se aplica un descuento del 5%, 
+            //entre $100 y $500 un descuento del 10%, y mayor a $500 un 20%.
+            //Informar el precio original, el descuento y el precio neto.
+
+            float importe;
+            float precioConDescuento;
+            float descuento;
+
+            Console.WriteLine("Ingrese el importe: ");
+            importe = float.Parse(Console.ReadLine());
+
+            if(importe<100)
+            {
+                descuento = importe * 0.05f;
+                precioConDescuento = importe - descuento;
+
+                Console.WriteLine("El importe $" + importe +" recibio un descuento de $" + 
+                descuento + " el importe con descuento es de $" + precioConDescuento);
+            }
+            else if(importe >= 100 && importe <= 500)
+            {
+                descuento = importe * 0.10f;
+                precioConDescuento = importe - descuento;
+
+                Console.WriteLine("El importe $" + importe +" recibio un descuento de $" + 
+                 descuento + " el importe con descuento es de $" + precioConDescuento);
+            }
+            else
+            {
+                descuento = importe * 0.20f;
+                precioConDescuento = importe - descuento;
+
+                Console.WriteLine("El importe $" + importe +" recibio un descuento de $" + 
+                 descuento + " el importe con descuento es de $" + precioConDescuento);
+            }
+        }
+    }
+}
---
+++ SueldoOperario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SueldoOperario
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            //Calcular el sueldo de un operario, ingresando la cantidad de horas trabajadas 
+            //y el valor que gana por hora. Si trabaja más de 40 hs, 
+            //las horas extras se pagan a razón de $1,50 el valor de la hora.
+            float horasTrabajadas;
+            float valorHora;
+            float sueldo;
+            float valorHorasExtras = 1.50f;
+            float sueldoHorasExtras;
+
+            Console.WriteLine("Ingrese las horas trabajadas: ");
+            horasTrabajadas = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingese el valor por hora de su salario: ");
+            valorHora = float.Parse(Console.ReadLine());
+
+            if(horasTrabajadas>40)
+            {
+                sueldoHorasExtras = (horasTrabajadas -40) * valorHorasExtras;
+                sueldo = (40 * valorHora) + sueldoHorasExtras;
+                Console.WriteLine("Su sueldo es de $" + sueldo);
+            }
+            else
+            {
+                sueldo = horasTrabajadas * valorHora;
+                Console.WriteLine("Su sueldo es de $" + sueldo);
+            }
+        }
+    }
+}
