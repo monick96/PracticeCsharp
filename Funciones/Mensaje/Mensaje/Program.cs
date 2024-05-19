028e1ed363541c6913f13670abb48010a3d8b20a
monica <monibrave1@gmail.com>
2024-05-19 00:39:27 -0300

    add new project in functions

---
+++ Funciones/Mensaje/Mensaje/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/*
+ 1. Armar una función, a la que se le pase 
+como parámetro un número N,
+y muestre por pantalla N veces el mensaje:
+“Módulo ejecutándose”.
+ */
+namespace Mensaje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            Console.WriteLine(mensaje(n));
+            Console.ReadKey();
+        }
+        static string mensaje(int n)
+        {
+            string msj = "";
+            for (int i = 1; i <= n; i++)
+            {
+                msj += "Modulo ejecutandose ";
+            }
+            return msj;
+            
+        }
+    }
+}
