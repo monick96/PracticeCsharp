818d70d849f33ce6de6d0de83d0254ed7206a6ef
monica <monibrave1@gmail.com>
2024-05-27 12:12:12 -0300

    modificate divisores primo projects

--- Funciones/DivisoresPrimos/DivisoresPrimos/Program.cs
+++ Funciones/DivisoresPrimos/DivisoresPrimos/Program.cs
@@ -18,10 +18,31 @@ namespace DivisoresPrimos
 
             int result = ObtenerCantDivisoresPrimos(num);
             Console.WriteLine($"El número {num} tiene {result} divisores primos.");
+           
+            Console.WriteLine(" Presione enter para pasar al siguiente punto...");
+            Console.ReadKey();
+            //fin punto 10
+
+            /////////////////////////////////////////////////////////////////////////////
+            /*11.  Crear una función a la que se le pasa un número entero 
+             * y devuelve un array con los divisores primos de dicho número.*/
+            ///////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Ingrese un entero: ");
+            num = int.Parse(Console.ReadLine());
+
+            int [] resultArray = obtenerArrayDivPrimos(num);
+
+            Console.WriteLine($"El número {num} tiene Los siguientes divisores primos: ");
+            for (int i = 0;i<resultArray.Length; i++)
+            {
+                if (resultArray[i] != 0)
+                    Console.WriteLine(resultArray[i]);
+            }
+
             Console.ReadKey();
         }
 
-        //funcion
+        // declaracion funcion
         static int ObtenerCantDivisoresPrimos(int num)
         {
 
@@ -57,46 +78,10 @@ namespace DivisoresPrimos
             }
 
             // Verifica si los divisores son primos
-            //i<position porque position tiene el tamaño del array con valores
+            //i < position porque position tiene el tamaño del array con valores
             for (int i = 0; i < position; i++)
             {
-                // Inicializa la bandera 'esPrimo' como verdadera
-                bool esPrimo = true;
-
-                // Si el divisor es 2, es primo
-                if (div[i] == 2)
-                {
-                    // Incrementa el contador de divisores primos
-                    contDivPrimo++;
-
-                    // Continua con el siguiente divisor
-                    //continue salta a la siguiente iteracion
-                    continue;
-                }
-
-                // Si el divisor es par y mayor que 2, no es primo
-                if (div[i] % 2 == 0)
-                {
-                    // Continua con el siguiente divisor
-                    //salta a la siguiente iteracion
-                    continue;
-                }
-
-                // Verifica la primalidad del divisor para los números impares
-                for (int j = 3; j < div[i] / 2; j += 2)
-                {
-                    // Si 'j' es un divisor de 'div[i]'
-                    if (div[i] % j == 0)
-                    {
-                        // Marca 'div[i]' como no primo
-                        esPrimo = false;
-                        // Sale del bucle
-                        break;
-                    }
-                }
-
-                // Si 'esPrimo' sigue siendo verdadero, 'div[i]' es primo
-                if (esPrimo)
+                if (EsPrimo(div[i]))
                 {
                     // Incrementa el contador de divisores primos
                     contDivPrimo++;
@@ -107,6 +92,72 @@ namespace DivisoresPrimos
             return contDivPrimo;
 
         
+        }
+        
+        static int[] obtenerArrayDivPrimos(int num)
+        {   //array a retornar tamaño n/2
+            int[] arrayPrimo = new int[num / 2];
+
+            // Declara un array para almacenar los divisores de
+            // 'num', con tamaño máximo de 'num / 2'
+            int[] div = new int[num / 2];
+
+            int position = 0;
+
+            // Añade el propio número si es mayor que 1
+            // (caso del número primo divisible solo x si mismo)
+            if (num > 1)
+            {
+                div[position] = num;
+                position++;
+            }
+
+            for (int i = 2; i <= num / 2; i++)
+            {
+                // Si 'i' es un divisor de 'num'
+                if (num % i == 0)
+                {
+                    // Almacena 'i' en el array de divisores
+                    div[position] = i;
+
+                    // Incrementa la posición del array
+                    position++;
+                }
+            }
+
+            // Verifica si los divisores son primos
+            //i < position porque position tiene el tamaño del array con valores
+            for(int i = 0; i < position;i ++)
+            {
+                if (EsPrimo(div[i]))
+                {
+                    arrayPrimo[i]= div[i];
+                }
+            }
+            return arrayPrimo;
+        }
+
+       
+        
+        static bool EsPrimo(int num)
+        {
+            // Los números menores o iguales a 1 no son primos
+            if (num <= 1) return false;
+
+            // 2 es el único número primo par
+            if (num == 2) return true;
+
+            // Números pares mayores que 2 no son primos
+            if (num % 2 == 0) return false; 
+
+            // Revisa los números impares desde 3 hasta la mitad del número
+            for (int i = 3; i <= (num/2); i += 2)
+            {
+                if (num % i == 0) return false; // Si num es divisible por i, no es primo
+            }
+
+            // Si no se encontró ningún divisor, es primo
+            return true; 
         }
 
     }
