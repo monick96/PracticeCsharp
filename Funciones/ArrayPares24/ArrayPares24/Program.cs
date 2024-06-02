f3a0fd1f9ca585078517e6fc15a6000f26a8feb6
monica <monibrave1@gmail.com>
2024-06-02 15:27:47 -0300

    add more exercises

---
+++ Funciones/ArrayPares24/ArrayPares24/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPares24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 24.Escribir una función a la que se le pasa como parámetro 
+             * un array que debe rellenar. Se leerá por teclado una serie 
+             * de números: guardar en el array solo los pares e ignorar los impares.
+             * También hay que devolver la cantidad de impares ignorados.
+             */
+            int[] nums;
+            int arrayT;
+            int cantIgnorado;
+
+            Console.WriteLine("Ingrese el tamaño que quiere darle al array: ");
+            arrayT = int.Parse(Console.ReadLine());
+            nums = new int[arrayT];
+
+            //rellenar array y devolver cant ignorados(impares)
+            cantIgnorado = RelllenarArrayPares(ref nums);
+
+            //mostrar ignorados
+            Console.WriteLine($"Se ignoraron {cantIgnorado} numeros ");
+            //mostrar array
+            MostrarArray(nums);
+
+            Console.ReadKey();
+
+        }
+        
+        //funciones
+        static int RelllenarArrayPares(ref int[] nums)
+        {
+            int cont = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"Ingrese el numero para la posicion {i + 1}");
+                int num = int.Parse(Console.ReadLine());
+                if (Espar(num))
+                {
+                    nums[i] = num;
+                }
+                else
+                {
+                    cont++;
+                }
+
+            }
+
+            return cont;
+        }
+
+        static void MostrarArray(int[] array)
+        {
+            int contCero = 0;
+            Console.WriteLine("Los valores del array son : ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] != 0)
+                {
+                    Console.WriteLine(array[i]);
+                }
+                else
+                {
+                    contCero++;
+                }
+            }
+
+            if(contCero == array.Length)
+                Console.WriteLine("No se ingresaron numeros pares");
+
+        }
+      
+        static bool Espar(int n)
+        {
+            bool par = false;
+
+            if (n%2 == 0)
+            {
+                par = true;
+            }
+
+            return par;
+        }
+    }
+}
---
+++ Funciones/DistanciaEuclidea23/DistanciaEuclidea23/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DistanciaEuclidea23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*23.  	Escribir una función que calcule la distancia 
+             * euclídea entre dos puntos del plano. Tener en cuenta que 
+             * la función debe recibir 4 parámetros: las 2 coordenadas 
+             * del primer punto y las 2 coordenadas del segundo punto.
+             * d= raiz2((x2-x1)2+(y2-y1)2)
+             */
+            //A=(1,2) y B = (4, 6)
+            //Console.WriteLine(CalcularDistanciaEuclidea(1,2,4,6));
+            float x1;
+            float y1;
+            float x2;
+            float y2;
+            float r;
+
+            Console.WriteLine("Distancia Euclidea entre 2 puntos...");
+
+            Console.WriteLine("Ingrese valor para x1: ");
+            x1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese valor para y1: ");
+            y1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese valor para x2: ");
+            x2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese valor para y2: ");
+            y2 = float.Parse(Console.ReadLine());
+
+            r =CalcularDistanciaEuclidea(x1,y1,x2,y2);
+
+            Console.WriteLine($"La distancia entre ({x1},{y1}) y ({x2},{y2}) es {r}");
+
+            Console.ReadKey();
+        }
+
+        static float CalcularDistanciaEuclidea(float x1, float y1, float x2, float y2)
+        {
+            float result;
+
+            result = (float) Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2 - y1,2));
+
+            return result;
+        }
+    }
+}
