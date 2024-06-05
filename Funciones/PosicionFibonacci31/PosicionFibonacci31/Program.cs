aa51af66a2bcfe554671e281dc228c2294ad6ac7
monica <monibrave1@gmail.com>
2024-06-05 12:23:49 -0300

    add more exercises

---
+++ Funciones/PosicionFibonacci31/PosicionFibonacci31/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PosicionFibonacci31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*31.Escribir una función que devuelva el número de la posición Fibonacci.*/
+            /**/
+            int num;
+            Console.WriteLine("valor de la posicion Fibonacci");
+            Console.WriteLine("Ingrese la posicion: ");
+            num = int.Parse(Console.ReadLine());
+            int[] arrayResult = SecuenciaFibonacci(num);
+
+            Console.WriteLine($"El valor de la posicion {num}, es: {arrayResult[num]} ");
+            Console.ReadKey();
+        }
+
+        static int[] SecuenciaFibonacci(int n) 
+        {
+            //sumamos uno a la posicion por que el conteo inicia en cero
+            int[] secuenciaResult = new int[n + 1];
+            
+            //inicializo las pociciones 0 y 1
+            secuenciaResult[0] = 0;
+            secuenciaResult[1] = 1;
+
+            //para las siguintes posiciones sumo las dos ultimas en bucle
+            //hasta llegar a la posicion solicitada
+            for (int i = 2; i < secuenciaResult.Length; i++)
+            {
+                secuenciaResult[i] = secuenciaResult[i - 1] + secuenciaResult[i - 2];
+            }
+
+            return secuenciaResult;
+        }
+    }
+}
---
+++ Funciones/SecuenciaCollatz32/SecuenciaCollatz32/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SecuenciaConjeturaCollatz32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*32.Escribir una función que dado un número natural,
+             * escriba por pantalla su secuencia de la Conjetura de Collatz 
+             * y que devuelva la longitud o cantidad de pasos que tiene 
+             * hasta llegar al primer 1.*/
+
+            /*1.Empieza con cualquier número entero positivo.
+              *2.Si es par, divídelo entre 
+              *3. Si es impar, multiplícalo por 3 y súmale 1.
+               *4.Repite el proceso con el número obtenido.
+                *La conjetura dice que siempre llegarás a 1, sin importar con qué número empieces.*/
+
+            uint num;
+            uint pasos;
+            Console.WriteLine("Secuencia de Collatz");
+            Console.WriteLine("Ingrese un numero natural...");
+            num = uint.Parse(Console.ReadLine());
+
+            Msg($"Inicio: {num}");
+            pasos = CantidadPasoSecuenciaCollatz(num);
+          
+            Console.ReadKey();
+        }
+        static uint CantidadPasoSecuenciaCollatz(uint num)
+        {
+            uint cantPasos = 0;
+
+            while (num > 1)
+            {
+                if (num % 2 == 0)
+                {
+                    num = num / 2;
+                    cantPasos++;
+                    Msg($"paso {cantPasos} es: {num} ");
+                }
+                else
+                {
+                    num = num * 3 + 1;
+                    cantPasos++;
+                    Msg($"paso {cantPasos} es: {num} ");
+                }
+
+            }
+
+            Msg($"Cantidad total de pasos: {cantPasos}");
+            return cantPasos;
+        }
+
+        static void Msg(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
