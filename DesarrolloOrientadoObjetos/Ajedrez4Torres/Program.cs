de924d42d2567ab0b3ea02c7728f00d36eff5ae2
Monica <monibrave1@gmail.com>
2024-11-10 20:40:52 -0300

    ajedrez iniciado

--- DesarrolloOrientadoObjetos/Ajedrez4Torres/Program.cs
+++ DesarrolloOrientadoObjetos/Ajedrez4Torres/Program.cs
@@ -15,7 +15,7 @@ namespace Ajedrez4Torres
         private int fila;
         private char columna;
         private int numeroTorre;
-        private string color;
+        private string color ; 
 
         public Torre(string color,int numeroTorre, char columna, int fila)
         {
@@ -25,12 +25,40 @@ namespace Ajedrez4Torres
             this.numeroTorre = numeroTorre;
         }
 
+        public int GetNumeroTorre()
+        {
+            return this.numeroTorre;
+        }
+
+        public string GetColorTorre()
+        {
+            return this.color;
+        }
+
         public void CambiarPosicion(int fila, char columna)
         {
-            this.fila = fila;
-            this.columna = columna;
+            if (this.MovimientoEsValido(fila, columna))
+            {
+                this.fila = fila;
+                this.columna = columna;
+                System.Console.WriteLine("Cambio la posicion de la torre");
+                this.MostrarPosicion();
+            }else
+            {
+                System.Console.WriteLine($"Movimiento invalido en fila o columna {this.fila}{this.columna}");
+            }  
+        }
 
-            this.MostrarPosicion();
+        public bool MovimientoEsValido(int fila, char columna)
+        {
+            bool valido = false;
+
+            if (this.fila == fila || this.columna == columna)
+            {
+                valido = true;
+            }
+
+            return valido;
         }
 
         public void MostrarPosicion()
@@ -42,9 +70,30 @@ namespace Ajedrez4Torres
 
     class Jugador 
     {
-        private string nombre;
+        private string nombre {get;}
         private List<Torre> torres;
-        private int cantidadTorres;
+        private int cantidadTorres {get; set;}
+
+        public Jugador (string nombre)
+        {
+            this.nombre = nombre;
+            this.cantidadTorres = 2;
+            this.torres = new List<Torre>();
+        }
+
+        public void AgregarTorres(Torre torre)
+        {
+            if ( this.torres.LongCount()<3)
+            {
+                this.torres.Add(torre);
+                System.Console.WriteLine($"Torre {torre.GetNumeroTorre} {torre.GetColorTorre} agregada");
+            }else
+            {
+                System.Console.WriteLine("Ya no se pueden agregar mas torres");
+            }
+        }
+
+
 
     }
 }
